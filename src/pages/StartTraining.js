8feb3b110da1bbb8c39df820b0899cdd494dc1bf
Fai <shingfai.lo.852.work@gmail.com>
2024-07-18 00:13:38 +0800

    at least can play the wav file

--- src/pages/StartTraining.js
+++ src/pages/StartTraining.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AppContext } from "../App";
@@ -8,161 +8,165 @@ import * as noteData from "../utils/noteData";
 import * as noteDataUtil from "../utils/noteDataUtil";
 import '../styles/MyComponent.css';
 import { MidiPlayer } from "react-midi-player";
+import audioFile from '../sound/GrandPiano/piano-f-c4.wav'
 
 const StartTraining = () => {
 
-  const { state } = useLocation();
-  const navigate = useNavigate();
-  const [selectedButton, setSelectedButton] = useState([]);
-  const [trainingQuestions, setTrainingQuestions] = useState([]);
-  const [trainingAnswers, seTrainingAnswers] = useState([]);
+    const {state} = useLocation();
+    const navigate = useNavigate();
+    const [selectedButton, setSelectedButton] = useState([]);
+    const [trainingQuestions, setTrainingQuestions] = useState([]);
+    const [trainingAnswers, seTrainingAnswers] = useState([]);
 
-  useEffect(() => {
-    generateTrainingQuestions();
-  }, [state.selectedNoteIndex]);
+    const [isPlaying, setIsPlaying] = useState(false);
 
-  // const questions
-  let currentQuestionIndex = 1; 
+    useEffect(() => {
+        generateTrainingQuestions();
+    }, [state.selectedNoteIndex]);
 
-  const generateTrainingQuestions = () => {
-    
-    const selectedNoteIndex = state.selectedNoteIndex;
-    const randomQuestions = [];
+    // const questions
+    let currentQuestionIndex = 1;
 
-    for (let i = 0; i < state.numOfAnswers; i++) {
-      const randomIndex = Math.floor(Math.random() * selectedNoteIndex.length);
-      randomQuestions.push(selectedNoteIndex[randomIndex]);
-    }
-    console.log("generateTrainingQuestions state.numOfAnswers:" + state.numOfAnswers);
-    console.log("generateTrainingQuestions randomQuestions:" + randomQuestions);
-    setTrainingQuestions(randomQuestions);
-  };
+    const generateTrainingQuestions = () => {
 
-  const handleBack = () => {
-    navigate("/settings");
-  };
+        const selectedNoteIndex = state.selectedNoteIndex;
+        const randomQuestions = [];
 
-
-  const eraseAnswerFunction = () => {
-    // Make a copy of the trainingAnswers array
-    const updatedTrainingAnswers = [...trainingAnswers];
-  
-    // Remove the last element from the copy
-    updatedTrainingAnswers.pop();
-  
-    // Update the trainingAnswers state with the modified array
-    seTrainingAnswers(updatedTrainingAnswers);
-  };
-
-  const finalAnswerFunction = () => {
-  };
-  
-  const playAllQuestionFunction = (trainingQuestions) => {
-    return () => {
-      const midiNotes = [
-        { note: 60, duration: 0.5, delay: 0 },
-        { note: 64, duration: 0.5, delay: 0.5 },
-        { note: 67, duration: 0.5, delay: 1 },
-      ];
-  
-      const player = new MidiPlayer.instruments.Player((event) => {
-        // Callback function to handle MIDI events
-        if (event.name === "noteOn") {
-          // Handle note on event
-        } else if (event.name === "noteOff") {
-          // Handle note off event
+        for (let i = 0; i < state.numOfAnswers; i++) {
+            const randomIndex = Math.floor(Math.random() * selectedNoteIndex.length);
+            randomQuestions.push(selectedNoteIndex[randomIndex]);
         }
-      });
-  
-      player.on("endOfFile", () => {
-        // Handle end of file event
-      });
-  
-      player.loadArrayBuffer(midiNotes);
-      player.play();
+        console.log("generateTrainingQuestions state.numOfAnswers:" + state.numOfAnswers);
+        console.log("generateTrainingQuestions randomQuestions:" + randomQuestions);
+        setTrainingQuestions(randomQuestions);
     };
-  };
 
-  const handleAnswer = (answer) => {
-    seTrainingAnswers([...trainingAnswers, answer]);
-  };
+    const handleBack = () => {
+        navigate("/settings");
+    };
 
-  const initialQuestions = [
-      {
-        question: 'Question:',
-        answer: trainingQuestions.map((item, index) =>
-          noteDataUtil.getNoteName(item, "solfege", state.selectedAccidentalsType)
-        ).join(", ")
-      },
-      {
-        question: 'Action:',
-        answer: <button id="playAllQuestionButton" onClick={playAllQuestionFunction(trainingQuestions)}>Play Again</button>
-      },
-      {
-          question: 'Answer:', 
-          answer: trainingAnswers.map((item, index) =>
-            noteDataUtil.getNoteName(item, "solfege", state.selectedAccidentalsType)
-          ).join(", ")
-      },
-      {
-        question: '', 
-        answer: 
-        <div>
-          <button id="eraseButton" onClick={eraseAnswerFunction}>Erase</button>
-          <button id="finalAnswerButton" onClick={finalAnswerFunction}>Final Answer</button>
-        </div>
-      },
-      {
-          question: '',
-          answer: (
-            <div>
-              {state.selectedNoteIndex.map((item, index) => (
-                <button
-                  key={item}
-                  className={`note-button ${selectedButton === item ? 'selected' : ''}`}
-                  onMouseDown={() => setSelectedButton(item)}
-                  onMouseUp={() => setSelectedButton(null)}
-                  onClick={() => handleAnswer(item)}
-                >
-                  {noteDataUtil.getNoteName(item, "solfege", state.selectedAccidentalsType)}
+
+    const eraseAnswerFunction = () => {
+        // Make a copy of the trainingAnswers array
+        const updatedTrainingAnswers = [...trainingAnswers];
+
+        // Remove the last element from the copy
+        updatedTrainingAnswers.pop();
+
+        // Update the trainingAnswers state with the modified array
+        seTrainingAnswers(updatedTrainingAnswers);
+    };
+
+    const finalAnswerFunction = () => {
+    };
+
+    const playAllQuestionFunction = () => {
+        // const audioFile = "../sound/GrandPiano/piano-f-c4.wav";
+        const audio = new Audio(audioFile);
+
+        if (audio) {
+            if (isPlaying) {
+                audio.pause();
+            } else {
+                audio.play();
+            }
+            setIsPlaying(!isPlaying);
+        }
+
+        // audio.play()
+        //     .then(() => {
+        //         console.log("Audio playback started successfully");
+        //     })
+        //     .catch((error) => {
+        //         console.error("Error playing audio:", error);
+        //     });
+    };
+
+    const handleAnswer = (answer) => {
+        seTrainingAnswers([...trainingAnswers, answer]);
+    };
+
+    const initialQuestions = [
+        {
+            question: 'Question:',
+            answer: trainingQuestions.map((item, index) =>
+                noteDataUtil.getNoteName(item, "solfege", state.selectedAccidentalsType)
+            ).join(", ")
+        },
+        {
+            question: 'Action:',
+            answer:
+            // <button id="playAllQuestionButton" onClick={playAllQuestionFunction(trainingQuestions)}>Play Again</button>
+
+                <button onClick={playAllQuestionFunction}>
+                    {isPlaying ? 'Pause' : 'Play'}
                 </button>
-              ))}
-            </div>
-          ),
-          // , answer: 
-          //   <div>
-          //     {state.selectedNoteIndex.map((item, index) => (
-          //         <button
-          //             key={item}
-          //             onClick={() => handleAnswer(item)}
-          //             style={{
-          //                 backgroundColor: "white",
-          //                 color: "black",
-          //                 width: "70px",
-          //                 padding: "8px",
-          //                 border: "1px solid #ccc",
-          //                 borderRadius: "4px",
-          //             }}
-          //         >
-          //             {noteDataUtil.getNoteName(item, "solfege", state.selectedAccidentalsType)}
-          //         </button>
-          //     ))}
-          //   </div>
-      },
-  ];
+        },
+        {
+            question: 'Answer:',
+            answer: trainingAnswers.map((item, index) =>
+                noteDataUtil.getNoteName(item, "solfege", state.selectedAccidentalsType)
+            ).join(", ")
+        },
+        {
+            question: '',
+            answer:
+                <div>
+                    <button id="eraseButton" onClick={eraseAnswerFunction}>Erase</button>
+                    <button id="finalAnswerButton" onClick={finalAnswerFunction}>Final Answer</button>
+                </div>
+        },
+        {
+            question: '',
+            answer: (
+                <div>
+                    {state.selectedNoteIndex.map((item, index) => (
+                        <button
+                            key={item}
+                            className={`note-button ${selectedButton === item ? 'selected' : ''}`}
+                            onMouseDown={() => setSelectedButton(item)}
+                            onMouseUp={() => setSelectedButton(null)}
+                            onClick={() => handleAnswer(item)}
+                        >
+                            {noteDataUtil.getNoteName(item, "solfege", state.selectedAccidentalsType)}
+                        </button>
+                    ))}
+                </div>
+            ),
+            // , answer:
+            //   <div>
+            //     {state.selectedNoteIndex.map((item, index) => (
+            //         <button
+            //             key={item}
+            //             onClick={() => handleAnswer(item)}
+            //             style={{
+            //                 backgroundColor: "white",
+            //                 color: "black",
+            //                 width: "70px",
+            //                 padding: "8px",
+            //                 border: "1px solid #ccc",
+            //                 borderRadius: "4px",
+            //             }}
+            //         >
+            //             {noteDataUtil.getNoteName(item, "solfege", state.selectedAccidentalsType)}
+            //         </button>
+            //     ))}
+            //   </div>
+        },
+    ];
 
-  return (
-    <Layout>
-      <QATable title="Melody Dictation Training" initialQuestions={initialQuestions}/>
+    return (
+        <Layout>
+            <QATable title="Melody Dictation Training" initialQuestions={initialQuestions}/>
 
 
-      {<h3>{"debug selectedNoteIndex:" + state.selectedNoteIndex.join(", ")}</h3>}
-      {<h3>{"debug selectedAccidentalsType:" + state.selectedAccidentalsType}</h3>}
+            {<h3>{"debug selectedNoteIndex:" + state.selectedNoteIndex.join(", ")}</h3>}
+            {<h3>{"debug selectedAccidentalsType:" + state.selectedAccidentalsType}</h3>}
 
-      <button onClick={handleBack}>Back</button>
+            <button onClick={handleBack}>Back</button>
 
-    </Layout>
-  );
+        </Layout>
+    );
 };
 
 export default StartTraining;

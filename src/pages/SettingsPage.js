426f4fd2a45fe2d0e14a56fbf9da50aab305f812
Fai <shingfai.lo.852.work@gmail.com>
2024-07-19 22:17:08 +0800

    change the numOfQuestions and numOfAnswers to dropdown

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -9,86 +9,115 @@ import * as noteDataUtil from "../utils/noteDataUtil";
 import QATable from '../components/QATable';
 
 const SettingsPage = () => {
-  const {
-    selectedNoteIndex,
-    setSelectedNoteIndex,
-    selectedAccidentalsType,
-    setSelectedAccidentalsType,
-    numOfQuestions,
-    setNumOfQuestions,
-    numOfAnswers,
-    setNumOfAnswers
-  } = useContext(AppContext);
+    const {
+        selectedNoteIndex,
+        setSelectedNoteIndex,
+        selectedAccidentalsType,
+        setSelectedAccidentalsType,
+        numOfQuestions,
+        setNumOfQuestions,
+        numOfAnswers,
+        setNumOfAnswers
+    } = useContext(AppContext);
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const noteChoiceIndex = [-12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    const noteChoiceIndex = [-12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-  const handleSelectNote = (index) => {
-    if (selectedNoteIndex.includes(index)) {
-      setSelectedNoteIndex(
-        selectedNoteIndex.filter((item) => item !== index),
-      );
-    } else {
-      setSelectedNoteIndex([...selectedNoteIndex, index].sort((a, b) => a - b));
-    }
-  };
+    const handleSelectNote = (index) => {
+        if (selectedNoteIndex.includes(index)) {
+            setSelectedNoteIndex(
+                selectedNoteIndex.filter((item) => item !== index),
+            );
+        } else {
+            setSelectedNoteIndex([...selectedNoteIndex, index].sort((a, b) => a - b));
+        }
+    };
 
-  const toggleSelectedAccidentalsType = () => {
-    setSelectedAccidentalsType((prevType) => (prevType === noteData.accidentalsType.sharp ? noteData.accidentalsType.flat : noteData.accidentalsType.sharp));
-  };
+    const toggleSelectedAccidentalsType = () => {
+        setSelectedAccidentalsType((prevType) => (prevType === noteData.accidentalsType.sharp ? noteData.accidentalsType.flat : noteData.accidentalsType.sharp));
+    };
 
-  const handleStartTraining = () => {
-    navigate("/start-training", { state: { selectedNoteIndex, selectedAccidentalsType, numOfQuestions, numOfAnswers} });
-  };
+    const handleStartTraining = () => {
+        navigate("/start-training", { state: { selectedNoteIndex, selectedAccidentalsType, numOfQuestions, numOfAnswers} });
+    };
 
-  const headers = ["Setting", "Choice"];
+    const headers = ["Setting", "Choice"];
 
-  const initialQuestions = [
-      {
-          question: 'Note Choice:'
-          , answer:
-              <div>
-                  {noteChoiceIndex.map((item, index) => (
-                      <React.Fragment key={item}>
-                          <button
-                              id={`noteChoiceButton${item}`}
-                              onClick={() => handleSelectNote(item)}
-                              style={{
-                                  backgroundColor: selectedNoteIndex.includes(item) ? 'blue' : 'white',
-                                  color: selectedNoteIndex.includes(item) ? 'white' : 'black',
-                                  width: '70px',
-                                  padding: '8px',
-                                  border: '1px solid #ccc',
-                                  borderRadius: '4px',
-                                  marginRight: index % 12 !== 11 ? '8px' : '0',
-                                  marginBottom: index % 12 === 11 ? '8px' : '0',
-                              }}
-                          >
-                              {noteDataUtil.getNoteName(item, 'solfege', selectedAccidentalsType)}
-                          </button>
-                          {index % 12 === 11 && <br/>}
-                      </React.Fragment>
-                  ))}
-              </div>
-      },
-      {
-          question: 'Accidentals Type'
-          , answer:
-              <button onClick={toggleSelectedAccidentalsType} style={{marginTop: "16px"}}>
-                  {noteDataUtil.getAccidentalsType(selectedAccidentalsType)}
-              </button>
-      },
-      {
-          question: 'How many questions'
-          , answer: numOfQuestions
-      },
-      {
-          question: 'How many notes for each questions'
-          , answer: numOfAnswers
-      },
-  ];
+    const MIN_QUESTIONS = 1;
+    const MAX_QUESTIONS = 20;
 
+    const MIN_ANSWERS = 1;
+    const MAX_ANSWERS = 20;
+
+    const initialQuestions = [
+        {
+            question: 'Note Choice:'
+            , answer:
+                <div>
+                    {noteChoiceIndex.map((item, index) => (
+                        <React.Fragment key={item}>
+                            <button
+                                id={`noteChoiceButton${item}`}
+                                onClick={() => handleSelectNote(item)}
+                                style={{
+                                    backgroundColor: selectedNoteIndex.includes(item) ? 'blue' : 'white',
+                                    color: selectedNoteIndex.includes(item) ? 'white' : 'black',
+                                    width: '70px',
+                                    padding: '8px',
+                                    border: '1px solid #ccc',
+                                    borderRadius: '4px',
+                                    marginRight: index % 12 !== 11 ? '8px' : '0',
+                                    marginBottom: index % 12 === 11 ? '8px' : '0',
+                                }}
+                            >
+                                {noteDataUtil.getNoteName(item, 'solfege', selectedAccidentalsType)}
+                            </button>
+                            {index % 12 === 11 && <br/>}
+                        </React.Fragment>
+                    ))}
+                </div>
+        },
+        {
+            question: 'Accidentals Type'
+            , answer:
+                <button onClick={toggleSelectedAccidentalsType} style={{marginTop: "16px"}}>
+                    {noteDataUtil.getAccidentalsType(selectedAccidentalsType)}
+                </button>
+        },
+        {
+            question: 'How many questions'
+            , answer: (
+                <select
+                    value={numOfQuestions}
+                    onChange={(e) => setNumOfQuestions(parseInt(e.target.value))}
+                    style={{marginTop: "16px"}}
+                >
+                    {Array.from({ length: MAX_QUESTIONS - MIN_QUESTIONS + 1 }, (_, i) => i + MIN_QUESTIONS).map((num) => (
+                        <option key={num} value={num}>
+                            {num}
+                        </option>
+                    ))}
+                </select>
+            )
+        },
+        {
+            question: 'How many notes for each questions'
+            , answer: (
+                <select
+                    value={numOfAnswers}
+                    onChange={(e) => setNumOfAnswers(parseInt(e.target.value))}
+                    style={{marginTop: "16px"}}
+                >
+                    {Array.from({ length: MAX_ANSWERS - MIN_ANSWERS + 1 }, (_, i) => i + MIN_ANSWERS).map((num) => (
+                        <option key={num} value={num}>
+                            {num}
+                        </option>
+                    ))}
+                </select>
+            )
+        },
+    ];
 
     return (
         <Layout>
@@ -99,9 +128,9 @@ const SettingsPage = () => {
             {/*<h3>{"debug selectedAccidentalsType:" + selectedAccidentalsType}</h3>*/}
             {/*<h3>{"debug noteChoiceIndex to name:" +*/}
             {/*    noteChoiceIndex.map((item, index) => {*/}
-          {/*        return noteDataUtil.getFlatSolfegeName(item);*/}
-          {/*    })*/}
-          {/*}</h3>*/}
+            {/*        return noteDataUtil.getFlatSolfegeName(item);*/}
+            {/*    })*/}
+            {/*}</h3>*/}
 
           <button onClick={handleStartTraining} style={{marginTop: "16px"}}>
               Start Training
